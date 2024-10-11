0a05745f45df3760f4f030869f73d2f83854a1a2
Сергей Морозов <Tyuiu.MorozovMS@outlook.com>
2024-10-11 20:05:25 +0500

    Task2

---
+++ Tyuiu.MorozovSm.Sprint3.Task2.V6.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.MorozovSm.Sprint3.Task2.V6.Lib;
+
+namespace Tyuiu.MorozovSm.Sprint3.Task2.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        DataService ds = new DataService();
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 10;
+            var res = ds.GetMultiplySeries(value,startValue, stopValue);
+            Assert.AreEqual(0, res);
+        }
+    }
+}
